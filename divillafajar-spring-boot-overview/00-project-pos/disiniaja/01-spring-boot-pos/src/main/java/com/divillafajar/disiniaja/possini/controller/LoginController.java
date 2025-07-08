5a36f5f919688b11f376a11a246c55ffcc3c32e4
Fajar Setiawan <fsetiaw@gmail.com>
2025-07-09 01:33:24 +0700

    01 pos project

---
+++ divillafajar-spring-boot-overview/00-project-pos/disiniaja/01-spring-boot-pos/src/main/java/com/divillafajar/disiniaja/possini/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.divillafajar.disiniaja.possini.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/disiniLoginPage")
+    public String showMyLoginPage() {
+        return "login-disini";
+    }
+}
---
+++ divillafajar-spring-boot-overview/00-project-pos/disiniaja/01-spring-boot-pos/src/main/java/com/divillafajar/disiniaja/possini/security/MainSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.divillafajar.disiniaja.possini.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MainSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails me = User.builder()
+                .username("me").password("{noop}me")
+                .roles("EMPLOYEE").build();
+
+        UserDetails you = User.builder()
+                .username("you").password("{noop}you")
+                .roles("EMPLOYEE","MANAGER").build();
+
+        UserDetails we = User.builder()
+                .username("we").password("{noop}we")
+                .roles("EMPLOYEE","MANAGER","ADMIN").build();
+
+        return new InMemoryUserDetailsManager(me,you,we);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/disiniLoginPage")
+                                .loginProcessingUrl("/autehnticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+        return httpSecurity.build();
+    }
+}
