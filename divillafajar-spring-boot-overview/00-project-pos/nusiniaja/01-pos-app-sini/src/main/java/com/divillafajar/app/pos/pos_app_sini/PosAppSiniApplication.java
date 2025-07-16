6f78c9bb043838875dc1541a76e8ec658e7ee7f3
Fajar Setiawan <fsetiaw@gmail.com>
2025-07-16 13:06:50 +0700

    Remove submodule nusiniaja and add as regular folder

---
+++ divillafajar-spring-boot-overview/00-project-pos/nusiniaja/01-pos-app-sini/src/main/java/com/divillafajar/app/pos/pos_app_sini/PosAppSiniApplication.java
@@ -0,0 +1,13 @@
+package com.divillafajar.app.pos.pos_app_sini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAppSiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAppSiniApplication.class, args);
+	}
+
+}
